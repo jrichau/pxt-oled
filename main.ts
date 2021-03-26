43ac5bbf8564dddbe0bee6910606c635fded0594
Josh Richau <jrichau@gmail.com>
2021-03-26 14:41:27 -0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,152 @@
-forever(function () {
-	
-})
+/**
+* makecode I2C OLED 128x32 Package.
+*/
+
+//% weight=20 color=#0855AA icon="O" block="OLED_I2C"
+namespace OLED_I2C {
+    let _I2CAddr = 0;
+    let _screen = pins.createBuffer(513);
+    let _buf2 = pins.createBuffer(2);
+    let _buf3 = pins.createBuffer(3);
+    let _buf4 = pins.createBuffer(4);
+    let _ZOOM = 0;
+
+    function cmd1(d: number) {
+        let n = d % 256;
+        pins.i2cWriteNumber(_I2CAddr, n, NumberFormat.UInt16BE);
+    }
+
+    function cmd2(d1: number, d2: number) {
+        _buf3[0] = 0;
+        _buf3[1] = d1;
+        _buf3[2] = d2;
+        pins.i2cWriteBuffer(_I2CAddr, _buf3);
+    }
+
+    function cmd3(d1: number, d2: number, d3: number) {
+        _buf4[0] = 0;
+        _buf4[1] = d1;
+        _buf4[2] = d2;
+        _buf4[3] = d3;
+        pins.i2cWriteBuffer(_I2CAddr, _buf4);
+    }
+
+    function set_pos(col: number = 0, page: number = 0) {
+        cmd1(0xb0 | page) // page number
+        let c = col * (_ZOOM + 1)
+        cmd1(0x00 | (c % 16)) // lower start column address
+        cmd1(0x10 | (c >> 4)) // upper start column address    
+    }
+
+    // clear bit
+    function clrbit(d: number, b: number): number {
+        if (d & (1 << b))
+            d -= (1 << b)
+        return d
+    }
+
+    /**
+     * draw / redraw screen
+     */
+    //% blockId="OLED_I2C_DRAW" block="draw"
+    //% weight=64 blockGap=8
+    //% parts=OLED_I2C trackArgs=0
+    export function draw() {
+        set_pos()
+        pins.i2cWriteBuffer(_I2CAddr, _screen)
+    }
+
+    /**
+     * clear screen
+     */
+    //% blockId="OLED_I2C_CLEAR" block="clear"
+    //% weight=63 blockGap=8
+    //% parts=OLED_I2C trackArgs=0
+    export function clear() {
+        _screen.fill(0)
+        _screen[0] = 0x40
+        draw()
+    }
+
+    /**
+     * turn on screen
+     */
+    //% blockId="OLED_I2C_ON" block="turn on"
+    //% weight=62 blockGap=8
+    //% parts=OLED_I2C trackArgs=0
+    export function on() {
+        cmd1(0xAF)
+    }
+
+    /**
+     * turn off screen
+     */
+    //% blockId="OLED_I2C_OFF" block="turn off"
+    //% weight=61 blockGap=8
+    //% parts=OLED_I2C trackArgs=0
+    export function off() {
+        cmd1(0xAE)
+    }
+
+    /**
+     * set pixel in OLED
+     * @param x is X alis, eg: 0
+     * @param y is Y alis, eg: 0
+     * @param color is dot color, eg: 1
+     */
+    //% blockId="OLED_I2C_PIXEL" block="set pixel at x %x|y %y|color %color"
+    //% weight=70 blockGap=8
+    //% parts=OLED_I2C trackArgs=0
+    export function pixel(x: number, y: number, color: number = 1) {
+        let zm = 0
+        let page = y >> 3
+        let shift_page = y % 8
+        let ind = x * (zm + 1) + page * 128 + 1
+        let b = (color) ? (_screen[ind] | (1 << shift_page)) : clrbit(_screen[ind], shift_page)
+        _screen[ind] = b
+        set_pos(x, page)
+        if (zm) {
+            _screen[ind + 1] = b
+            _buf3[0] = 0x40
+            _buf3[1] = _buf3[2] = b
+            pins.i2cWriteBuffer(_I2CAddr, _buf3)
+        }
+        else {
+            _buf2[0] = 0x40
+            _buf2[1] = b
+            pins.i2cWriteBuffer(_I2CAddr, _buf2)
+        }
+    }
+
+    /**
+     * OLED initialize
+     * @param addr is i2c addr, eg: 60
+     */
+    //% blockId="OLED_I2C_init" block="init OLED with addr %addr"
+    //% weight=100 blockGap=8
+    //% parts=OLED_I2C trackArgs=0
+    export function init(addr: number) {    
+        _I2CAddr = addr;
+        cmd1(0xAE)       // SSD1306_DISPLAYOFF
+        cmd1(0xA4)       // SSD1306_DISPLAYALLON_RESUME
+        cmd2(0xD5, 0xF0) // SSD1306_SETDISPLAYCLOCKDIV
+        cmd2(0xA8, 0x3F) // SSD1306_SETMULTIPLEX
+        cmd2(0xD3, 0x00) // SSD1306_SETDISPLAYOFFSET
+        cmd1(0 | 0x0)    // line #SSD1306_SETSTARTLINE
+        cmd2(0x8D, 0x14) // SSD1306_CHARGEPUMP
+        cmd2(0x20, 0x00) // SSD1306_MEMORYMODE
+        cmd3(0x21, 0, 127) // SSD1306_COLUMNADDR
+        cmd3(0x22, 0, 7)  // SSD1306_PAGEADDR
+        cmd1(0xa0 | 0x1) // SSD1306_SEGREMAP
+        cmd1(0xc8)       // SSD1306_COMSCANDEC
+        cmd2(0xDA, 0x02) // SSD1306_SETCOMPINS
+        cmd2(0x81, 0xCF) // SSD1306_SETCONTRAST
+        cmd2(0xd9, 0xF1) // SSD1306_SETPRECHARGE
+        cmd2(0xDB, 0x40) // SSD1306_SETVCOMDETECT
+        cmd1(0xA6)       // SSD1306_NORMALDISPLAY
+        cmd2(0xD6, 1)    // zoom on
+        cmd1(0xAF)       // SSD1306_DISPLAYON
+        clear()
+        _ZOOM = 0
+    }
+}
